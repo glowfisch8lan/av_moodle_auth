c5e4d5f1b236bf177ec1956c92eca2ee1e14a1c5
glowfisch <glowfisch8lan@gmail.com>
2022-07-05 10:28:41 +1000

    post-install-cmd

---
+++ src/Moodle.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace app\custom\models\auth;
+
+/**
+ * Class Moodle
+ * @package app\custom\models\auth
+ *
+ * Авторизация с помощью внешнего сервера Moodle | Файл-заглушка
+ */
+class Moodle extends \Glfs\Auth\Moodle
+{
+}
